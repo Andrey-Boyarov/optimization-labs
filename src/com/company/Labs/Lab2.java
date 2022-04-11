369905695b2d12db8ad3bd4d86497950cc1e1115
boyarov.a <boyarov.a@it-universe.ru>
2022-04-11 13:37:40 +0400

    dimensions are coming

--- src/com/company/Labs/Lab2.java
+++ src/com/company/Labs/Lab2.java
@@ -43,4 +43,64 @@ public class Lab2 {
 
         return x_1.plus(x_0).times(0.5);
     }
+
+    public static DimPoint perCoordDescend(Function<DimPoint, Double> f, DimPoint x_start, double eps, int max_iters)throws Exception
+    {
+        DimPoint x_0 = new DimPoint(x_start);
+
+        DimPoint x_1 = new DimPoint(x_start);
+
+        double step = 1.0;
+
+        double x_i, y_1, y_0;
+
+        int opt_coord_n = 0, coord_id;
+
+        int i = 0;
+
+        for (i = 0; i < max_iters; i++)
+        {
+            coord_id = i % x_0.size();
+
+            x_1.set(coord_id, x_1.get(coord_id) - eps);
+
+            y_0 = f.apply(x_1);
+
+            x_1.set(coord_id, x_1.get(coord_id) + 2 * eps);
+
+            y_1 = f.apply(x_1);
+
+            x_1.set(coord_id, x_1.get(coord_id) - eps);
+
+            x_1.set(coord_id,y_0 > y_1 ? x_1.get(coord_id) + step : x_1.get(coord_id) - step);
+
+            x_i = x_0.get(coord_id);
+
+            x_1 = dichotomy(f, x_0, x_1, max_iters, eps);
+
+            x_0 = new DimPoint(x_1);
+
+            if (Math.abs(x_1.get(coord_id) - x_i) < eps)
+            {
+                opt_coord_n++;
+
+                if (opt_coord_n == x_1.size())
+                {
+                    return x_0;
+                }
+                continue;
+            }
+            opt_coord_n = 0;
+        }
+
+        return x_0;
+    }
+    public static DimPoint perCoordDescend(Function<DimPoint, Double> f, DimPoint x_start, double eps)throws Exception
+    {
+        return     perCoordDescend( f,  x_start,  eps, 1000);
+    }
+    public static DimPoint perCoordDescend(Function<DimPoint, Double> f, DimPoint x_start)throws Exception
+    {
+        return     perCoordDescend( f,  x_start,  1e-5,1000);
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,5 +14,11 @@ public class Main {
         Lab1.fibonacci(x -> x * x + x,-10,10,15);
 
         Lab2.dichotomy(point -> (point.get(0) - 2) * (point.get(0) - 2) + (point.get(1) - 2) * (point.get(1) - 2), new DimPoint(0d, 0d), new DimPoint(5d, 5d), 100, 1e-8);
+
+        try {
+            Lab2.perCoordDescend(point -> (point.get(0) - 5) * point.get(0) + (point.get(1) - 3) * point.get(1), new DimPoint(-13d, 22d));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
